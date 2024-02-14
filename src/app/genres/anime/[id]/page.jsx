6f1954c75153330a71b86ce01c966a6196cf0c63
Ryuzu <kanimenia778@gmail.com>
2024-02-14 15:20:31 +0700

    Updated

--- src/app/genres/anime/[id]/page.jsx
+++ src/app/genres/anime/[id]/page.jsx
@@ -1,27 +1,56 @@
+"use client"
+import React, { useEffect, useState } from 'react';
 import AnimeList from "@/components/AnimeList";
 import { getAnimeResponse } from "@/libs/api-libs";
 
-const Page = async ({ params: { id } }) => {
-  // Dapatkan respons anime dari sumber data
-  const animeResponse = await getAnimeResponse("anime");
-  const genreResponse = await getAnimeResponse("genres/anime")
+const Page = ({ params: { id } }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [allAnime, setAllAnime] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const [genreName, setGenreName] = useState('');
 
-  const genreName = genreResponse.data.find(genre => genre.mal_id === parseInt(id))?.name;
-  
-  let filteredAnime = animeResponse.data.filter(anime => {
-    // Cek apakah anime memiliki genre dengan id yang sesuai
-    return anime.genres.some(genre => genre.mal_id === parseInt(id));
-  });
+  useEffect(() => {
+    const fetchAnime = async () => {
+      setIsLoading(true);
+      const animeResponse = await getAnimeResponse("anime", `page=${currentPage}`);
+      const genreResponse = await getAnimeResponse("genres/anime");
+      const currentGenre = genreResponse.data?.find(genre => genre.mal_id === parseInt(id))?.name;
+      setGenreName(currentGenre);
+      // Check if animeResponse.data is iterable before setting state
+      if (Array.isArray(animeResponse.data)) {
+        setAllAnime(prevAnime => [...prevAnime, ...animeResponse.data]);
+        setIsLoading(false);
+        setHasMore(animeResponse.data.length > 0);
+      } else {
+        console.error("animeResponse.data is not an iterable object");
+        setIsLoading(false);
+        setHasMore(false);
+      }
+    };
 
-  console.log(animeResponse.data)
-  
-  // Lakukan pemfilteran anime berdasarkan genres
-  filteredAnime = { data: filteredAnime }
+    fetchAnime();
+  }, [currentPage, id]);
+
+  useEffect(() => {
+    const loadNextPage = () => {
+      if (isLoading || !hasMore) return;
+      setCurrentPage(prevPage => prevPage + 1);
+    };
+
+    const timer = setTimeout(loadNextPage, 700); // Load next page after 2 seconds
+
+    return () => clearTimeout(timer);
+  }, [isLoading, hasMore]);
+
+  const filteredAnime = allAnime.filter(anime => anime.genres.some(genre => genre.mal_id === parseInt(id)));
 
   return (
     <div>
       <h1 className="font-bold text-2xl flex items-center justify-center py-4">Anime Genre {genreName}</h1>
-      <AnimeList api={filteredAnime}/>
+      <AnimeList api={{ data: filteredAnime }} />
+      {isLoading && <p>Loading...</p>}
+      {!hasMore && <p>Anime Tidak Didapatkan</p>}
     </div>
   );
 }
